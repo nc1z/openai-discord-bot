6313737ee1deb7e978cb10d5ab9e55bf56ccdb7f
nc1z <ncxwork@gmail.com>
2023-06-19 13:49:03 +0800

    feat: added discord client event handlers

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import dotenv from "dotenv"
+import { Client } from "discord.js"
 import initializeServer from "./server"
 import initClient from "./clients/discord-client"
+import registerEventHandlers from "./events/registerEventHandlers"
 
 dotenv.config()
 
 const main = async () => {
-    const client = initClient()
+    const client: Client<boolean> = initClient()
     const PORT: number = Number(process.env.PORT) || 3001
     initializeServer(PORT)
+    registerEventHandlers(client)
 }
 
 main().catch((error) => {
---
+++ src/types/enum/ErrorMessage.ts
@@ -0,0 +1,8 @@
+const enum ErrorMessage {
+    RESPONSE_EMPTY = `Sorry, something went wrong and the response is empty. Please try again!`,
+    RESPONSE_ERROR = `Sorry, there was an error fetching the response.`,
+    RESPONSE_GENERIC = `Sorry, something went wrong. Please try again later!`,
+    RESPONSE_MAINTENANCE = `MAINTENANCE: ongoing updates. Please try again later.`,
+  }
+  
+export default ErrorMessage
