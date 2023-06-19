c9c3fccc7d68c64f0ec0f9da1b243403bafe9180
nc1z <ncxwork@gmail.com>
2023-06-19 15:58:31 +0800

    chore: fix eslint warnings

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { Routes } from 'discord.js'
 import { REST } from '@discordjs/rest'
 import { clientId, guildId, token } from './config.json'
@@ -5,25 +6,20 @@ import fs from 'node:fs'
 import path from 'node:path'
 
 const commands = []
-// Grab all the command files from the commands directory you created earlier
 const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'))
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`)
 	commands.push(command.data.toJSON())
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token)
 
-// and deploy your commands!
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`)
 
-		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
